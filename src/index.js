1b780aa4317d527ecdf7f89daf39e740d76efb85
yzwang-13 <53463432+yzwang-13@users.noreply.github.com>
2019-12-09 13:31:34 +1100

    final

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const cunstomStyle = {
+  color: "red",
+  fontSize: "20px",
+  border: "1px solid black"
+};
+
+// can change style on the go
+// cunstomStyle.color = "blue";
+
+ReactDOM.render(
+  <h1 style={cunstomStyle}>Hello World!</h1>,
+  document.getElementById("root")
+);
